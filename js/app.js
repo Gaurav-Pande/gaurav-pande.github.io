b37b97d1768959b97613edf7cf64c347ad5530a5
Gauravpande <garvpande11235@gmail.com>
2019-05-24 20:11:18 +0530

    adding slider

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(window).load(function() {
+    $('.flexslider').flexslider({
+    animation: "fade",
+    directionNav: true,
+    slideshowSpeed: 5000,
+    animationSpeed: 600,
+    touch: true
+    });
+});
